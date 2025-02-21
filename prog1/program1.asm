af9a94ffeb26514a4751387255456ece49a38aaf
Will Gresham <willgresham34@gmail.com>
2025-02-20 21:29:01 -0500

    prog 1 and 2 done

--- prog1/program1.asm
+++ prog1/program1.asm
@@ -3,6 +3,7 @@
 a_arr:      .word   10, 5, 6, 15, 17, 19, 25, 30, 12, 56    # array a
 b_arr:      .word   4, 15, 16, 5, 7, 9, 15, 10, 22, 6       # array b
 c_arr:      .space  40                                      # init array c with 40 bytes
+arr_length: .word   10
 res_msg:    .asciiz "Result is: "
 space:      .asciiz " "
 
@@ -13,12 +14,13 @@ main:
     la      $t0,        a_arr                               # load pointer to a
     la      $t1,        b_arr                               # load pointer to b
     la      $t2,        c_arr                               # load pointer to c
+    lw      $t3,        arr_length
 
     li      $s0,        0                                   # init i to 0
 
 Loop:
 
-    beq     $s0,        10,         PrintMsg                # if i = 10 got to done
+    beq     $s0,        $t3,        PrintMsg                # if i = 10 got to done
 
     lw      $s1,        0($t0)                              # load value from a[i]
     lw      $s2,        0($t1)                              # load value from b[i]
@@ -44,7 +46,7 @@ PrintMsg:
 
 PrintArr:
 
-    beq     $s0,        10,         Done                    # if at end of array move to Done
+    beq     $s0,        $t3,        Done                    # if at end of array move to Done
 
     lw      $a0,        0($t2)                              # load number into $a0
     li      $v0,        1
@@ -62,147 +64,6 @@ PrintArr:
 Done:
     li      $v0,        10
     syscall
-
-
-
-
-
-.data
-
-a_arr:      .word   10, 5, 6, 15, 17, 19, 25, 30, 12, 56    # array a
-b_arr:      .word   4, 15, 16, 5, 7, 9, 15, 10, 22, 6       # array b
-c_arr:      .space  40                                      # init array c with 40 bytes
-res_msg:    .asciiz "Result is: "
-cs:         .asciiz ", "
-
-.text
-main:
-
-
-    la      $t0,        a_arr                               # load pointer to a
-    la      $t1,        b_arr                               # load pointer to b
-    la      $t2,        c_arr                               # load pointer to c
-
-    li      $s0,        0                                   # init i to 0
-
-Loop:
-
-    beq     $s0,        10,         PrintMsg                # if i = 10 got to done
-
-    lw      $s1,        0($t0)                              # load value from a[i]
-    lw      $s2,        0($t1)                              # load value from b[i]
-
-    sub     $s3,        $s1,        $s2                     # a[i] - b[i]
-    sw      $s3,        0($t2)                              # store value to c[i] mem address
-
-    addi    $t0,        $t0,        4                       # get next index for a
-    addi    $t1,        $t1,        4                       # get next index for b
-    addi    $t2,        $t2,        4                       # get next index for c
-    addi    $s0,        $s0,        1                       # i++
-
-    j       Loop                                            # jump back  to start
-
-PrintMsg:
-
-    la      $a0,        res_msg                             # put message in $a0
-    li      $v0,        4                                   # print msg
-    syscall
-
-    la      $t2,        c_arr                               # Rest t2 to start of array
-    li      $s0,        0                                   #rest i to 0
-
-PrintArr:
-
-    beq     $s0,        10,         Done                    # if at end of array move to Done
-
-    sll     $t3,        $s0,        2                       # get offset value
-    add     $t2,        $t2,        $t3                     # create mem address for c[i]
-    lw      $a0,        0($t2)                              # load number into $a0
-    li      $v0,        1
-    syscall
-
-    la      $a0,        cs                                  # put comma space in $a0
-    li      $v0,        4                                   # print comma space
-    syscall
-
-    addi    $s0,        $s0,        1                       # i++
-    j       PrintArr
-
-Done:
-    li      $v0,        10
-    syscall
-
-
-
-
-
-
-.data
-
-a_arr:      .word   10, 5, 6, 15, 17, 19, 25, 30, 12, 56    # array a
-b_arr:      .word   4, 15, 16, 5, 7, 9, 15, 10, 22, 6       # array b
-c_arr:      .space  40                                      # init array c with 40 bytes
-res_msg:    .asciiz "Result is: "
-cs:         .asciiz ", "
-
-.text
-main:
-
-
-    la      $t0,        a_arr                               # load pointer to a
-    la      $t1,        b_arr                               # load pointer to b
-    la      $t2,        c_arr                               # load pointer to c
-
-    li      $s0,        0                                   # init i to 0
-
-Loop:
-
-    beq     $s0,        10,         PrintMsg                # if i = 10 got to done
-
-    lw      $s1,        0($t0)                              # load value from a[i]
-    lw      $s2,        0($t1)                              # load value from b[i]
-
-    sub     $s3,        $s1,        $s2                     # a[i] - b[i]
-    sw      $s3,        0($t2)                              # store value to c[i] mem address
-
-    addi    $t0,        $t0,        4                       # get next index for a
-    addi    $t1,        $t1,        4                       # get next index for b
-    addi    $t2,        $t2,        4                       # get next index for c
-    addi    $s0,        $s0,        1                       # i++
-
-    j       Loop                                            # jump back  to start
-
-PrintMsg:
-
-    la      $a0,        res_msg                             # put message in $a0
-    li      $v0,        4                                   # print msg
-    syscall
-
-    la      $t2,        c_arr                               # Rest t2 to start of array
-    li      $s0,        0                                   #rest i to 0
-
-PrintArr:
-
-    beq     $s0,        10,         Done                    # if at end of array move to Done
-
-    sll     $t3,        $s0,        2                       # get offset value
-    add     $t2,        $t2,        $t3                     # create mem address for c[i]
-    lw      $a0,        0($t2)                              # load number into $a0
-    li      $v0,        1
-    syscall
-
-    la      $a0,        cs                                  # put comma space in $a0
-    li      $v0,        4                                   # print comma space
-    syscall
-
-    addi    $s0,        $s0,        1                       # i++
-    j       PrintArr
-
-Done:
-    li      $v0,        10
-    syscall
-
-
 
 
 
--- prog2/program2.asm
+++ prog2/program2.asm
@@ -1,19 +1,44 @@
 .data
 arr:        .word   10, 3, 45, 90, 12
-msg_res:    .asciiz "Largest in given array is"
+arr_length: .word   5
+res_msg:    .asciiz "Largest in given array is "
 
 .text
 main:
 
-    li      $t0,    0                           # init i
-    la      $t1,    arr                         # load array
-    lw      $s0,    0($t1)                      # get initial max value
+    li      $t0,    1                               # init i
+    la      $t1,    arr                             # load array
+    lw      $t3,    arr_length                      # get arr_length
+    lw      $s0,    0($t1)                          # get initial max value
 
 Loop:
-    beq     $t0,    5,      PrintMax            # if at end of array
+    beq     $t0,    $t3,        PrintMax            # if at end of array length 5
 
+    lw      $s1,    4($t1)                          # get a[i]
+    bgt     $s1,    $s0,        SetMax              # if $s1 > $s0 then goto target
 
+    addi    $t1,    $t1,        4                   #increment arr
+    addi    $t0,    $t0,        1                   # i++
 
+    j       Loop
 
+SetMax:
+    move    $s0,    $s1                             # store new max in $s0
+
+    addi    $t1,    $t1,        4                   #increment arr
+    addi    $t0,    $t0,        1                   # i++
+
+    j       Loop
 
 PrintMax:
+
+    la      $a0,    res_msg                         # put message in $a0
+    li      $v0,    4                               # print msg
+    syscall
+
+    move    $a0,    $s0                             # copy result to $a0
+    li      $v0,    1                               # print value
+    syscall
+
+    li      $v0,    10
+    syscall
